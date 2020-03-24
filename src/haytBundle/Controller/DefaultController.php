8dcddcb954e1ac184a96e0adb057996d1eb146f4
aminegongi <48979759+aminegongi@users.noreply.github.com>
2020-03-24 20:42:58 +0100

    firstCommit

---
+++ src/haytBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace haytBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('@hayt/Default/haytFrontHome.html.twig');
+    }
+}
---
+++ src/haytBundle/haytBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace haytBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class haytBundle extends Bundle
+{
+}
