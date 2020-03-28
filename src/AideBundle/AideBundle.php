eea9f695155fc96c46dfe7e251be125101d52039
hedi-khalifa <hedi.khalifa1@esprit.tn>
2020-03-28 01:33:08 +0100

    Creating AideBundle

---
+++ src/AideBundle/AideBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AideBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AideBundle extends Bundle
+{
+}
---
+++ src/AideBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AideBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('@Aide/Default/index.html.twig');
+    }
+}
