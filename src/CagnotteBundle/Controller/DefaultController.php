45be9f8174dfdc1ed0e2746f91846ed8b824506f
AhmedZeghibi <ahmed.zeghibi@esprit.tn>
2020-03-30 09:12:44 +0100

    A fix to CagnotteBundle

--- src/CagnotteBundle/Controller/DefaultController.php
+++ src/CagnotteBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CagnotteBundle:Default:index.html.twig');
+        return $this->render('@Cagnotte/Default/index.html.twig');
     }
 }
