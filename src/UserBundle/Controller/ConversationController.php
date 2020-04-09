c5ca0171efc55aaa454088981810c30f4ad73795
aminegongi <48979759+aminegongi@users.noreply.github.com>
2020-04-09 19:04:16 +0200

    chat client ema mezel fih chwaya _Amine

--- src/UserBundle/Controller/ConversationController.php
+++ src/UserBundle/Controller/ConversationController.php
@@ -58,25 +58,54 @@ class ConversationController extends Controller
             'ch'=>$code
         ));
     }
-/*
-<!-- Sender Message-->
-<div class="media w-50 mb-3"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
-<div class="media-body ml-3">
-<div class="bg-light rounded py-2 px-3 mb-2">
-<p class="text-small mb-0 text-muted">Test, which is a new approach to have</p>
-</div>
-<p class="small text-muted">12:00 PM | Aug 13</p>
-</div>
-</div>
 
-<!-- Reciever Message-->
-<div class="media w-50 ml-auto mb-3">
-<div class="media-body">
-<div class="bg-primary rounded py-2 px-3 mb-2">
-<p class="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
-</div>
-<p class="small text-muted">12:00 PM | Aug 13</p>
-</div>
-</div>
-*/
+    public function getlistConvAction(Request $req)
+    {
+        /*
+        $me = $this->container->get('security.token_storage')->getToken()->getUser();
+        $idme=$me->getId();
+        $ar = $this->getDoctrine()->getRepository(Conversation::class)->getlistConDQL($idme);
+        $code='';
+        foreach ($ar as $conv){
+            $img = $conv->getSender()->getImage();
+            $code .= '<div class="media w-50 mb-3"><img src="../../uploads/UserImg/'.$img.'" alt="user" width="50" class="rounded-circle">';
+            $code .= '<div class="media-body ml-3"> <div class="bg-light rounded py-2 px-3 mb-2">';
+            $code .= '<p class="text-small mb-0 text-muted"> '.$conv->getMessage().'</p> </div>';
+            $code .= '<p class="small text-muted">'.$conv->getDateEnvoi()->format('d M | H:i').'</p></div></div>';
+        }
+        */
+
+        $idr=2;
+        $me = $this->container->get('security.token_storage')->getToken()->getUser();
+        $idme=$me->getId();
+
+        $ar = $this->getDoctrine()->getRepository(Conversation::class)->getlistConDQL($idme);
+
+        $code='';
+        foreach ($ar as $conv ){
+                $img = $conv->getSender()->getImage();
+                $code .= '<a onclick=" to='.$conv->getSender()->getId().'" class="list-group-item list-group-item-action list-group-item-light rounded-0">';
+                $code .= '<div class="media"><img src="../../uploads/UserImg/'.$img.'" alt="user" width="50" class="rounded-circle">';
+                $code .= '<div class="media-body ml-4">';
+                $code .= '<div class="d-flex align-items-center justify-content-between mb-1">';
+                $code .= '<h6 class="mb-0">'.$conv->getSender()->getUsername().'</h6><small class="small font-weight">'.$conv->getDateEnvoi()->format('d M').'</small></div>';
+                $code .= '<p class="font-italic mb-0 text-small" style=" font-size: 12px; text-align: left;width: 150px;white-space: nowrap;overflow: hidden;text-overflow: ellipsis; " >'.$conv->getMessage().'</p></div></div></a>';
+        }
+
+        return new JsonResponse(array(
+            'conv'=>$code
+        ));
+    }
+/*
+ <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
+                            <div class="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
+                                <div class="media-body ml-4">
+                                    <div class="d-flex align-items-center justify-content-between mb-1">
+                                        <h6 class="mb-0">Jason Doe</h6><small class="small font-weight">02-06-2020</small>
+                                    </div>
+                                    <p class="font-italic mb-0 text-small" style=" font-size: 12px; text-align: left;width: 150px;white-space: nowrap;overflow: hidden;text-overflow: ellipsis; " >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                                </div>
+                            </div>
+                        </a>
+ */
 }
--- src/UserBundle/Repository/ConversationRepository.php
+++ src/UserBundle/Repository/ConversationRepository.php
@@ -35,6 +35,7 @@ class ConversationRepository extends \Doctrine\ORM\EntityRepository
         return $q->getResult();
     }
 
+    /*-----------------------------------------------------*/
     public function getSRConversation($rec,$sen)
     {
         $q=$this->getEntityManager()->createQuery(
@@ -43,16 +44,33 @@ class ConversationRepository extends \Doctrine\ORM\EntityRepository
         return $q->getResult();
     }
 
-    public function getInContact($re)
+
+
+    public function getInConversation($conna)
     {
-        $rec= $this->getReceiver($re);
-        $sen = $this->getSender($re);
-
-        $ic[] = array_merge($sen, $rec);
-        $ii = array_unique($ic);
-
-        return $sen;
+        $conn = $this->getEntityManager()->getConnection();
+        $sql="select Max(dateEnvoi) as dd ,message,sender,receiver,id from conversation where (sender=".$conna." or receiver=".$conna.") Group BY sender,receiver ORDER by dd DESC";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll();
     }
 
+    public function getAllConversation($mee)
+    {
+        $q=$this->getEntityManager()->createQuery(
+            "Select c , Max(c.dateEnvoi) as HIDDEN dd from UserBundle:Conversation c where (c.receiver='".$mee."' or c.sender='".$mee."') Group BY c.sender, c.receiver Order by dd DESC"
+        );
+        return $q->getResult();
+    }
 
+    public function getlistConDQL($conna)
+    {
+        $q=$this->getEntityManager()->createQuery(
+            "Select c , Max(c.dateEnvoi) as HIDDEN date from UserBundle:Conversation c where (c.receiver=".$conna." or c.sender=".$conna.") Group BY c.sender, c.receiver ORDER by date DESC"
+        );
+
+        return $q->getResult();
+    }
+
+    //"Select c , Max(c.dateEnvoi) as dd  from UserBundle:Conversation c where (c.receiver='$conna' or c.sender='$conna') Group BY c.sender, c.receiver ORDER by dd DESC"
 }
