b8b67a8594d1e166038f27719b9f16056ab3a01f
AhmedZeghibi <ahmed.zeghibi@esprit.tn>
2020-04-17 05:00:52 +0100

    [Cagnotte] Added role checks, integrity improvements, and added "takeAction"

--- src/CagnotteBundle/Controller/cagnotteController.php
+++ src/CagnotteBundle/Controller/cagnotteController.php
@@ -2,6 +2,7 @@
 
 namespace CagnotteBundle\Controller;
 
+use CagnotteBundle\CagnotteBundle;
 use CagnotteBundle\Entity\cagnotte;
 use CagnotteBundle\Form\cagnotteType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -12,14 +13,16 @@ class cagnotteController extends Controller{
     public function indexAction(){
         $tab=$this->getUser()->getRoles();
         $userRole=$tab[0];
-        if ($userRole == 'ROLE_US'){
+        $userId=$this->getUser()->getId();
+        if (($userRole == 'ROLE_US') || ($userRole == 'ROLE_ORG')){
             $em = $this->getDoctrine()->getManager();
 
             $cagnottes = $em->getRepository('CagnotteBundle:cagnotte')->findAll();
 
             return $this->render('@Cagnotte/Cagnotte/index.html.twig', array(
                 'cagnottes' => $cagnottes,
-                'userRole' => $userRole));
+                'userRole' => $userRole,
+                'userId' => $userId));
         }
         else{
             return $this->render('@Cagnotte/Cagnotte/error.html.twig');
@@ -29,13 +32,20 @@ class cagnotteController extends Controller{
     public function newAction(Request $request){
         $tab=$this->getUser()->getRoles();
         $userRole=$tab[0];
-        if ($userRole == 'ROLE_US'){
+        $userId=$this->getUser()->getId();
+        if (($userRole == 'ROLE_US') || ($userRole == 'ROLE_ORG')){
             $cagnotte = new cagnotte();
             $form = $this->createForm(cagnotteType::class, $cagnotte);
             $form->handleRequest($request);
 
             if ($form->isSubmitted()){
                 $em = $this->getDoctrine()->getManager();
+                $cagnotte->setDateDeCreation(new \DateTime('now'));
+                $cagnotte->setIdProprietaire($userId);
+                $cagnotte->setIdProprietaire($userId);
+                $cagnotte->setMontantActuel(0);
+                $cagnotte->setIdOrganisation(0);
+                $cagnotte->setEtat(0);
                 $em->persist($cagnotte);
                 $em->flush();
                 return $this->redirectToRoute('cagnotte_homepage');
@@ -50,7 +60,7 @@ class cagnotteController extends Controller{
     public function editAction(Request $request, $id){
         $tab=$this->getUser()->getRoles();
         $userRole=$tab[0];
-        if ($userRole == 'ROLE_US'){
+        if (($userRole == 'ROLE_US') || ($userRole == 'ROLE_ORG')){
             $em = $this->getDoctrine()->getManager();
             $cagnotte = $em->getRepository('CagnotteBundle:cagnotte')->find($id);
             $form = $this->createForm(cagnotteType::class, $cagnotte);
@@ -74,7 +84,7 @@ class cagnotteController extends Controller{
     public function deleteAction($id){
         $tab=$this->getUser()->getRoles();
         $userRole=$tab[0];
-        if ($userRole == 'ROLE_US'){
+        if (($userRole == 'ROLE_US') || ($userRole == 'ROLE_ORG')){
             $em = $this->getDoctrine()->getManager();
             $cagnotte = $em->getRepository('CagnotteBundle:cagnotte')->find($id);
             $em->remove($cagnotte);
@@ -89,7 +99,7 @@ class cagnotteController extends Controller{
     public function showAction($id){
         $tab=$this->getUser()->getRoles();
         $userRole=$tab[0];
-        if ($userRole == 'ROLE_US'){
+        if (($userRole == 'ROLE_US') || ($userRole == 'ROLE_ORG')){
             $em = $this->getDoctrine()->getManager();
 
             $selected = $em->getRepository('CagnotteBundle:cagnotte')->find($id);
@@ -115,11 +125,26 @@ class cagnotteController extends Controller{
         // Token is created using Stripe Checkout or Elements!
         // Get the payment token ID submitted by the form:
         $charge = \Stripe\Charge::create([
-            'amount' => 1000,
-            'currency' => 'eur',
+            'amount' => 5000,
+            'currency' => 'usd',
             'description' => 'Donation',
             'source' => $request->request->get('stripeToken'),
         ]);
+        return $this->redirectToRoute('cagnotte_homepage');
+    }
+
+    public function takeAction($id){
+
+        $tab=$this->getUser()->getRoles();
+        $userRole=$tab[0];
+        if ($userRole == 'ROLE_ORG'){
+            $em = $this->getDoctrine()->getManager();
+            $cagnotte = $em->getRepository('CagnotteBundle:cagnotte')->find($id);
+            $cagnotte->setEtat(1);
+            $cagnotte->setIdOrganisation($this->getUser()->getId());
+            $em->persist($cagnotte);
+            $em->flush();
+        }
         return $this->redirectToRoute('cagnotte_homepage');
     }
 }
